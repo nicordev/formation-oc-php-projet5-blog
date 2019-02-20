444e5be85de2fc8f42681627b61601f278bee656
SansGodasse <sansgodasse@gmail.com>
2019-02-20 15:40:00 +0100

    Add about me and portfolio posts

--- database/p5_demo.sql
+++ database/p5_demo.sql
@@ -252,13 +252,91 @@ INSERT INTO bl_post (p_id, p_author_id_fk, p_last_editor_id_fk, p_creation_date,
 VALUES
 (null, 1, null, '2018-09-24 12:15:35', null, 0, 'Article exemple', 'Un article du site.', '<h2>Un article.</h2><p>Tout simple.</p>'),
 (null, 1, 4, '2019-01-05 16:42:12', '2019-02-10 23:54:10', 0, 'Article modifié', 'Un autre article du site.', '<h2>Un article.</h2><p>Tout simple.</p>'),
-(null, 1, null, '2019-02-18 14:20:12', null, 1, 'Article écrit en Markdown', 'Un article écrit en **Markdown**.', 'Cet article contient du **Markdown** et c''est bien *sympa*.');
+(null, 1, null, '2019-02-18 14:20:12', null, 1, 'Article écrit en Markdown', 'Un article écrit en Markdown.', 'Cet article contient du **Markdown** et c''est bien *sympa*.'),
+(null, 3, null, '2019-02-02 10:52:45', null, 0, 'En bref', 'Je suis Nicolas Renvoisé, 31 ans à l''heure où j''écris cet article, et je me forme au métier de développeur backend avec l''aide d''OpenClassrooms. Pourquoi ? Parce que j''ai découvert que le code, j''adore ça !!!', '<h2>Comment j''en suis arriv&eacute; l&agrave; ?</h2>
+<p>Au d&eacute;part, j''ai fais 3 ans d''&eacute;tudes dans le domaine de l''eau et de l''environnement apr&egrave;s mon bac scientifique. A l''issue, je me suis engag&eacute; dans l''arm&eacute;e en tant que contr&ocirc;leur a&eacute;rien. Finalement, l''arm&eacute;e ce n''&eacute;tait pas aussi bien que ce que j''esp&eacute;rais et j''ai arr&ecirc;t&eacute; &agrave; la fin de mon contrat initial. J''ai alors int&eacute;gr&eacute; un bureau d''&eacute;tudes sp&eacute;cialis&eacute; dans l''eau et l''environnement o&ugrave; je me suis d&eacute;couvert une passion pour la programmation lorsque j''ai eu &agrave; faire des fichiers excel en VBA. Depuis ce jour, je n''arr&ecirc;te pas de coder !</p>
+<p>Un ami programmeur m''a conseill&eacute; de suivre les cours du site du z&eacute;ro sur le langage C pour d&eacute;buter sur de bonnes bases. C''est alors que j''ai d&eacute;couvert que le site du z&eacute;ro &eacute;tait devenu OpenClassrooms et je m''y suis inscrit.</p>
+<p>Apr&egrave;s le cours de Mathieu Nebra sur le C (un super cours, je le recommande !), j''ai enchain&eacute; sur les cours li&eacute;s au web, domaine qui m''intriguait beaucoup. Entre temps, j''en avais marre de mon travail au bureau d''&eacute;tudes (la bo&icirc;te &eacute;tait super, mais je n''aimais pas r&eacute;diger des rapports de 600 pages refoul&eacute;s pour un oui ou pour un non par l''administration...) et finalement j''ai d&eacute;cid&eacute; de partir pour me reconvertir dans le d&eacute;veloppement web en suivant la formation de d&eacute;veloppeur d''application PHP/Symfony d''OpenClassrooms.</p>
+<h2>Pourquoi cette formation et pas une autre ?</h2>
+<p>Une &eacute;cole Simplon s''&eacute;tait ouverte &agrave; 40 minutes de chez moi depuis 2 ans, mais rien n''indiquait que la formation gratuite qu''elle proposait aller &ecirc;tre reconduite. Du coup j''ai opt&eacute; pour OpenClassrooms en demandant un financement de la part de P&ocirc;le emploi, ce qui fut accept&eacute; ! Ouf ! Du coup non seulement j''ai l''avantage de suivre une formation reconnue par l''Etat, mais en plus je n''ai pas &agrave; me d&eacute;placer !</p>
+<h2>Comment se passe cette formation ?</h2>
+<p>Bien. Vraiment tr&egrave;s bien m&ecirc;me ! Je m''&eacute;clate tous les jours &agrave; programmer, d&egrave;s que je s&egrave;che je peux en parler soit avec mon mentor, soit avec les autres &eacute;tudiants d''OpenClassrooms. Je n''ai jamais aussi bien v&eacute;cu une formation.</p>'),
+(null, 3, null, '2019-02-03 11:00:01', null, 1, 'Pourquoi ce site ?', 'Ce site présente mes réalisations web, mon CV et des articles sur divers thèmes. Je l''ai créé pendant ma formation de développeur backend (c''est le projet 5).', 'Voici quelques liens utiles pour naviguer sur le site :
+
+* [Mon CV](/cv/cv.pdf)
+* [Mes réalisations](/blog?category-id=3)
+* [Le blog](/blog?category-id=1)'),
+(null, 3, null, '2019-01-06 16:23:14', null, 0, 'Mes sites web perso', 'Les 2 sites que j''ai réalisés avant ma formation de développeur backend, juste à l''aide des cours gratuits d''OpenClassrooms. Le premier est un site sur le barefoot running et le second est un site pour s''orienter en balade.', '<h2>Les sites web</h2>
+
+<div class="card-deck">
+    <div class="card">
+        <a class="card-body" href="https://sansgodasses.com">
+            Mon site sur le barefoot running : sansgodasses.com
+        </a>
+    </div>
+    <div class="card">
+        <a class="card-body" href="https://carte.ovh">
+            Mon site pour s''orienter en balade : carte.ovh
+        </a>
+    </div>
+</div>
+
+<h2>Pour info</h2>
+
+<p>
+    <a href="https://sansgodasses.com">sansgodasses.com</a> utilise une bibliothèque JavaScript pour traduire le Markdown.
+</p>
+<p>
+    <a href="https://carte.ovh">carte.ovh</a> utilise l''API de Google Maps et celle du géoportail.
+</p>'),
+(null, 3, null, '2019-01-07 14:51:11', null, 0, 'Mes projets réalisés lors de ma formation de développeur backend', 'Voici les liens vers les projets mis en ligne. Vous trouverez aussi les fichiers sources sur mon profil GitHub.', '<h2>Les projets en ligne</h2>
+
+<div class="card-deck">
+    <div class="card">
+        <a class="card-body" href="https://formation-oc-php.sansgodasses.com/projet-2/">
+            Intégrez un thème Wordpress pour un client
+        </a>
+    </div>
+    <div class="card">
+        <a class="card-body" href="https://formation-oc-php.sansgodasses.com/projet-3/">
+            Analysez les besoins de votre client pour son Festival de films
+        </a>
+    </div>
+    <div class="card">
+        <a class="card-body" href="https://formation-oc-php.sansgodasses.com/projet-4/">
+            Concevez la solution technique d''une application de restauration en ligne, Express Food (contient uniquement les diagrammes UML et le fichier .sql)
+        </a>
+    </div>
+    <div class="card">
+        <a class="card-body" href="/home">
+            Créez votre premier blog en PHP (c''est ce site)
+        </a>
+    </div>
+</div>
+
+<h2>A venir...</h2>
+
+<ul>
+    <li>Développez de A à Z le site communautaire SnowTricks</li>
+    <li>Créez un web service exposant une API</li>
+    <li>Améliorez une application existante de ToDo & Co</li>
+</ul>
+
+<h2>La formation OpenClassrooms</h2>
+
+<p>
+    <a href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">Contenu de la formation</a>
+</p>');
 
 INSERT INTO bl_post_tag (pt_post_id_fk, pt_tag_id_fk)
 VALUES
 (1, 4),
 (2, 4),
-(3, 4);
+(3, 4),
+(4, 1),
+(5, 2),
+(6, 3),
+(7, 3);
 
 INSERT INTO bl_comment (com_id, com_parent_id_fk, com_post_id_fk, com_author_id_fk, com_last_editor_id_fk, com_creation_date, com_last_modification_date, com_content, com_approved)
 VALUES
