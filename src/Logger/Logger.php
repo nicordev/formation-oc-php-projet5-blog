80c11dd115101b9d6e44cb0fcfcda924b009773e
Nicolas Renvoisé <nicolas.renvoise.dev@gmail.com>
2019-05-12 07:03:55 +0100

    Improve the contact message log system

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -9,14 +9,41 @@ class Logger
         // Disabled
     }
 
+    /**
+     * Add a log in /log/blog.log
+     *
+     * @param string $data
+     */
     public static function addLog(string $data)
     {
-        $data = date("Y-m-d H:i:s") . " client: " . ($_SERVER['REMOTE_ADDR'] ?? "") . " message: " . $data;
+        $data = self::addDateToData($data);
         file_put_contents(ROOT_PATH . "/log/blog.log", $data . "\n", FILE_APPEND);
     }
 
+    /**
+     * Save a message sent via the contact form in a log file
+     *
+     * @param string $name
+     * @param string $email
+     * @param string $message
+     */
     public static function saveContactMessage(string $name, string $email, string $message)
     {
-        file_put_contents(ROOT_PATH . "/log/contact_messages.log", "$name($email) sent a message: $message\n", FILE_APPEND);
+        $message = "$name($email) sent a message: $message\n";
+        $data = self::addDateToData($message);
+        file_put_contents(ROOT_PATH . "/log/contact_messages.log", $data, FILE_APPEND);
+    }
+
+    /**
+     * Add the current date to data (and the remote address)
+     *
+     * @param $data
+     * @return string
+     */
+    private static function addDateToData($data)
+    {
+        $data = date("Y-m-d H:i:s") . " client: " . ($_SERVER['REMOTE_ADDR'] ?? "") . " message: " . $data;
+
+        return $data;
     }
 }
